f084735bdfd23b9f612930de7fa3abac239c5bac
Alex Rabin <alexrabin@users.noreply.github.com>
2026-06-01 12:13:30 -0700

    Fixed riverpod tests

--- test/shared/widgets/shell/sidebar/sidebar_widget_test.dart
+++ test/shared/widgets/shell/sidebar/sidebar_widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/misc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pristine_cleaner/core/models/nav_item.dart';
 import 'package:pristine_cleaner/core/models/scan_view_model.dart';
--- test/test_support.dart
+++ test/test_support.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/misc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pristine_cleaner/core/models/file_item.dart';
 import 'package:pristine_cleaner/core/models/scan_result.dart';
